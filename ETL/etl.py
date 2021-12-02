58fc0dce9a0a208443c622f82a94ae575fb2fe84
vanithaasivakumar <30811623+vanithaasivakumar@users.noreply.github.com>
2021-12-02 15:31:30 -0500

    Updated files

--- ETL/etl.py
+++ ETL/etl.py
@@ -7,37 +7,37 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
-
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
+    df = pd.read_json(filepath, lines=True)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']].values.tolist()[0]
     cur.execute(artist_table_insert, artist_data)
 
+    # insert song record
+    song_data = df[['song_id','title','artist_id','year','duration']].values.tolist()[0]
+    cur.execute(song_table_insert, song_data)
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == "NextSong"]
 
     # convert timestamp column to datetime
-    t = 
+    t = df['ts']
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = ([(time, time.hour, time.day, time.week, time.month, time.year, time.dayofweek) for time in \
+                  [pd.to_datetime(line, unit='ms') for line in t]])
+    column_labels = ('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(time_data, columns=column_labels)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +56,8 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = [pd.to_datetime(row.ts, unit="ms"), row.userId, row.level, songid, artistid, row.sessionId,\
+                         row.location, row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
 
 
--- ETL/sql_queries.py
+++ ETL/sql_queries.py
@@ -1,30 +1,30 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE songsplay"
-user_table_drop = "DROP TABLE user"
-song_table_drop = "DROP TABLE song"
-artist_table_drop = "DROP TABLE artist"
-time_table_drop = "DROP TABLE time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE songplay(
-    songplay_id INT PRIMARY KEY
+songplay_table_create = ("""CREATE TABLE songplays(
+    songplay_id SERIAL PRIMARY KEY
     ,start_time TIMESTAMP
     ,user_id INT
     ,level VARCHAR(25)
-    ,song_id INT
-    ,artist_id INT
+    ,song_id VARCHAR(100)
+    ,artist_id VARCHAR(100)
     ,session_id INT
     ,location VARCHAR(255)
     ,user_agent VARCHAR(255)
-    FOREIGN KEY(star_time REFERENCES time(start_time))
-    FOREIGN KEY(user_id REFERENCES user(user_id))
-    FOREIGN KEY(song_id REFERENCES song(song_id))
-    FOREIGN KEY(artist_id REFERENCES artist(artist_id))
+    ,FOREIGN KEY(start_time) REFERENCES time(start_time)
+    ,FOREIGN KEY(user_id) REFERENCES users(user_id)
+    ,FOREIGN KEY(song_id) REFERENCES songs(song_id)
+    ,FOREIGN KEY(artist_id) REFERENCES artists(artist_id))
 """)
 
-user_table_create = ("""CREATE TABLE user(
+user_table_create = ("""CREATE TABLE users(
     user_id INT PRIMARY KEY
     ,first_name VARCHAR(100)
     ,last_name VARCHAR(100)
@@ -32,25 +32,25 @@ user_table_create = ("""CREATE TABLE user(
     ,level VARCHAR(25))
 """)
 
-artist_table_create = ("""CREATE TABLE artist(
-    artist_id INT PRIMARY KEY
+artist_table_create = ("""CREATE TABLE artists(
+    artist_id VARCHAR(100) PRIMARY KEY
     ,name VARCHAR(100)
     ,location VARCHAR(255)
     ,latitude NUMERIC
     ,longitude NUMERIC)
 """)
 
-song_table_create = ("""CREATE TABLE song(
-    song_id INT PRIMARY KEY
+song_table_create = ("""CREATE TABLE songs(
+    song_id VARCHAR(100) PRIMARY KEY
     ,title VARCHAR(255)
-    ,artist_id INT
+    ,artist_id VARCHAR(100)
     ,year INT
     ,duration NUMERIC
-    FOREIGNKEY(artist_id) REFERENCES artist(artist_id))
+    ,FOREIGN KEY(artist_id) REFERENCES artists(artist_id))
 """)
 
 time_table_create = ("""CREATE TABLE time(
-    start_time TIMESTAMP
+    start_time TIMESTAMP UNIQUE
     ,hour INT
     ,day INT
     ,week INT
@@ -61,28 +61,59 @@ time_table_create = ("""CREATE TABLE time(
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = ("""INSERT INTO songplays(
+start_time,
+user_id,
+level,
+song_id,
+artist_id,
+session_id,
+location,
+user_agent)
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+ON CONFLICT DO NOTHING
 """)
 
-user_table_insert = ("""
+user_table_insert = ("""INSERT INTO users(
+user_id,
+first_name,
+last_name,
+gender,
+level)
+VALUES (%s,%s,%s,%s,%s)
+ON CONFLICT(user_id) DO UPDATE SET LEVEL = EXCLUDED.LEVEL
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES (%s, %s, %s,%s, %s) ON CONFLICT DO NOTHING""")
+
+artist_table_insert = ("""INSERT INTO artists(
+artist_id,
+name,
+location,
+latitude,
+longitude)
+VALUES (%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING
 """)
 
-artist_table_insert = ("""
-""")
 
-
-time_table_insert = ("""
+time_table_insert = ("""INSERT INTO time(
+start_time,
+hour,
+day,
+week,
+month,
+year,
+weekday)
+VALUES(%s,%s,%s,%s,%s,%s,%s)
+ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
 
-song_select = ("""
+song_select = ("""SELECT song_id , artists.artist_id FROM songs JOIN artists ON songs.artist_id = artists.artist_id where title=%s AND name=%s AND duration=%s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, artist_table_create, song_table_create, time_table_create]
+create_table_queries = [time_table_create,artist_table_create,song_table_create,user_table_create,songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
